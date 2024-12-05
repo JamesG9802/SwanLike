1c52d4a89cf071a0a05812543ac10f0ee06e378d
JamesG9802 <61638683+JamesG9802@users.noreply.github.com>
2024-12-05 12:23:09 -0500

    Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,83 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import App from './App.tsx'
+import { createTheme, CssBaseline, StyledEngineProvider, ThemeProvider, useMediaQuery } from '@mui/material';
+
+const rootElement = document.getElementById("root");
+
+const darkTheme = createTheme({ 
+  palette: { mode: 'dark' },
+  typography: {
+      fontFamily: [
+          "Open Sans",
+          "Roboto",
+          "Helvetica",
+          "Arial",
+          "sans-serif",
+          "ui-sans-serif",
+          "system-ui",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji"
+      ].join(',')
+  },
+  //  Make portal elements inject into the root element
+  //  https://mui.com/material-ui/integrations/interoperability/#tailwind-css
+  components: {
+      MuiPopover: {
+          defaultProps: {
+            container: rootElement,
+          },
+        },
+      MuiPopper: {
+          defaultProps: {
+              container: rootElement,
+          },
+      },
+      MuiDialog: {
+          defaultProps: {
+              container: rootElement,
+          },
+      },
+      MuiModal: {
+          defaultProps: {
+              container: rootElement,
+          },
+      },
+  }
+});
+
+const lightTheme = createTheme({
+  ...darkTheme, 
+  palette: { mode: 'light' }
+});
+
+function Main() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = prefersDarkMode ? darkTheme : lightTheme;
+
+  useEffect(() => {
+      rootElement?.classList.add(prefersDarkMode ? "dark" : "light");
+      rootElement?.classList.remove(prefersDarkMode ? "light" : "dark")
+  }, [prefersDarkMode]);
+
+  return (
+    <>
+      <CssBaseline/>
+      <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <App/>
+          </ThemeProvider>
+      </StyledEngineProvider>
+    </>
+  );
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Main/>
   </StrictMode>,
 )
