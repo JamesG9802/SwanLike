55e8a1d5020165ee16452a800eba5d1a388e6fb0
JamesG9802 <61638683+JamesG9802@users.noreply.github.com>
2024-12-05 14:52:15 -0500

    Moving folders.

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,16 @@ function App() {
       <Canvas>
         <orthographicCamera/>
         <ambientLight intensity={0.1}/>
-        <directionalLight color={"red"} position={[0, 1, 1]}/>
+        <directionalLight color={"rgb(100, 0, 100)"} position={[0, 1, 1]}/>
+        <mesh position={[-3,0,0]} scale={[2, 2, 2]}>
+            <boxGeometry/>
+            <meshStandardMaterial attach='material' color={'white'}/>
+        </mesh>    
         <Box/>
+        <mesh position={[3,0,0]} scale={[2, 2, 2]}>
+            <boxGeometry/>
+            <meshStandardMaterial attach='material' color={'red'}/>
+        </mesh>    
         <Stats/>
         <OrbitControls/>
       </Canvas>
