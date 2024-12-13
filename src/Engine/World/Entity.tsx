88050fc001141ae93b3deb253efdd2a580ebe816
JamesG9802 <61638683+JamesG9802@users.noreply.github.com>
2024-12-13 12:00:42 -0500

    Expanding the Entity API and fixing lifecyles.
    World, Entity, and Component lifecycles have been updated to enforce better referencing.
    
    # New
    - Worlds now must link to the Three.js scene object.
    - Entities now keep track of newly added components.
    - Entities' components are now only accessible through `get_components()` and `get_components_iter()`.
    - Entities now store a map of component names to components to ensure only a single component of each type is kept.
    - Entities now have a dedicated `dispose` function.
    - Components now support deferred destruction.
    # Changed
    - `Identifiable.d.tsx` has been reverted back to `Identifiable.tsx`.
    - Old `Entity.cleanup()` has been renamed to `Entity.dispose()` to better reflect its functionality.
    - `World.get_entities()` now returns a readonly array.
    # Removed
    - Worlds no longer keep track of newly added entities.

--- src/Engine/World/Entity.tsx
+++ src/Engine/World/Entity.tsx
@@ -9,12 +9,45 @@ import { EntityConfig } from "Engine/Config/EntityConfig";
 
 /**
  * An entity is a basic Object3D with support for component scripts.
+ * 
+ * # Entity
+ * Based on GameObjects from Unity, entities are the base unit of worlds.
+ * 
+ * ## Event Lifecycle
+ * Entities have a similar event lifecycle to Unity GameObjects.
+ * 
+ * ### Start
+ * On creation, an entity invokes `start` on all `Component`s attached to it.
+ * @see {@link start}
+ * @see {@link Component.start}
+ * 
+ * ### Update
+ * When an entity updates, it first initializes any components that have been attached to it
+ * since the last time it has updated.
+ * Then it invokes `update` on all components attached to it.
+ * Finally, any components marked for destruction have `dispose` invoked and are removed.
+ * 
+ * ### Dispose
+ * When an entity's destruction is being finalized, it also invokes `dispose` on all components.
+ * 
+ * ## Compoent List Modification
+ * Additions to the component map are visible immediately.
+ * - Also, only one instance of a component type is allowed.
+ * Deletions to the component map are processed at the end of the update frame, so they can be referenced without worry.
+ * 
+ * @see {@link update}
+ * 
  */
 export class Entity extends THREE.Object3D {
     /**
+     * Components that will be added to the component list.
+     */
+    private future_components: Map<string, Component>;
+
+    /**
      * The components the entity has.
      */
-    components: Map<string, Component>;
+    private components: Map<string, Component>;
 
     /**
      * Whether the entity is going to be destroyed.
@@ -29,8 +62,41 @@ export class Entity extends THREE.Object3D {
         super();
         this.name = name;
 
+        this.future_components = new Map<string, Component>();
         this.components = new Map<string, Component>();
         this._death_flag = false;
+    }
+
+    /**
+     * Returns an iterator going through the entity's components.
+     * @returns
+     */
+    *get_components_iter(): IterableIterator<[string, Component]> {
+        // Iterate over entries in `this.components`
+        for (const [key, value] of this.components.entries()) {
+            yield [key, value];
+        }
+        // Iterate over entries in `this.future_components`
+        for (const [key, value] of this.future_components.entries()) {
+            yield [key, value];
+        }
+    }
+
+    /**
+     * Returns a dictionary of the entity's components.
+     * Note: this is NOT the entity's actual map of components, so while you can modify
+     * the components, modifying the map itself (by trying to add/remove components)
+     * does not affect the actual entity. 
+     * @returns 
+     */
+    get_components(): Map<string, Component> {
+        const dictionary: Map<string, Component> = new Map();
+        const iterator = this.get_components_iter();
+
+        for(const [key, value] of iterator) {
+            dictionary.set(key, value);
+        }
+        return dictionary;
     }
 
     /**
@@ -38,8 +104,25 @@ export class Entity extends THREE.Object3D {
      * @param components 
      */
     add_components(...components: Component[]) {
-        for (let i = 0; i < components.length; i++) {
-            this.components.set(components[i].name, components[i]);
+        for(let i = 0; i < components.length; i++) {
+            //  Component exists
+            if(this.components.has(components[i].name) || this.future_components.has(components[i].name)) {
+                log.warn(`Entity ${this.name} already has ${components[i].name}.`);
+            }
+            else {
+                this.future_components.set(components[i].name, components[i]);
+            }
+        }
+    }
+
+    /**
+     * Remove components to the entity.
+     * @param component_names the names of the component.   
+     */
+    remove_components(...component_names: string[]) {
+        for(let i = 0; i < component_names.length; i++) {
+            this.future_components.get(component_names[i])?.destroy();
+            this.components.get(component_names[i])?.destroy();
         }
     }
 
@@ -47,8 +130,10 @@ export class Entity extends THREE.Object3D {
      * Invoke all components that need to start.
      */
     start() {
-        this.components.forEach((component) => {
+        this.future_components.forEach((component, name) => {
             component.start();
+            this.components.set(name, component);
+            this.future_components.delete(name);
         });
     }
 
@@ -57,9 +142,39 @@ export class Entity extends THREE.Object3D {
      * @param delta - the time since the last frame
      */
     update(delta: number) {
+        this.future_components.forEach((component, name) => {
+            if(component.will_destroy()) {
+                return;
+            }
+            component.start();
+            this.components.set(name, component);
+            this.future_components.delete(name);
+        });
+
         this.components.forEach((component) => {
             if (component.active) {
                 component.update(delta);
+            }
+        });
+
+        this.cleanup();
+    }
+    
+    /**
+     * Remove all components that are marked for destruction.
+     */
+    protected cleanup() {
+        this.future_components.forEach((component) => {
+            if (component.will_destroy()) {
+                component.dispose();
+                this.future_components.delete(component.name);
+            }
+        });
+
+        this.components.forEach((component) => {
+            if (component.will_destroy()) {
+                component.dispose();
+                this.components.delete(component.name);
             }
         });
     }
@@ -79,7 +194,15 @@ export class Entity extends THREE.Object3D {
         return this._death_flag;
     }
 
-    cleanup() {
+    /**
+     * Cleanup all resources owned by this entity.
+     * NOTE: this should NOT be used for destroying an entity.
+     * @see {@link destroy} for destroying an entity in a safe manner.
+     */
+    dispose() {
+        this.future_components.forEach((component) => {
+            component.dispose();
+        });
         this.components.forEach((component) => {
             component.dispose();
         });
