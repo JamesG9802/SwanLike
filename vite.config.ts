88d62bdca157efc599985f06883122ef7acb5f6a
JamesG9802 <61638683+JamesG9802@users.noreply.github.com>
2024-12-13 14:18:06 -0500

    Fixing issue with 0.0.1 deployment.
    Somehow, EventHandler was in src/utility and not src/Utility.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths';
 import topLevelAwait from "vite-plugin-top-level-await";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
